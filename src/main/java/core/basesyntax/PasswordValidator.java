d75484b72127140b9b1921637ae35554e1226507
Bohdan Vorobiei <vbs@ukr.net>
2025-10-17 17:59:42 +0300

    remove encoding config from checkstyle plugin3

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -8,3 +8,5 @@ public class PasswordValidator {
         //write your code here
     }
 }
+
+
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -38,3 +38,4 @@ public class UserService {
         userService.registerUser(mismatchPassUser);
     }
 }
+
